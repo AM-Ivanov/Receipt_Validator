f45d0189890aadc1435f9a5c87d9b2b76f1dbeea
AM-Ivanov <this.teemo.baby@gmaail.com>
2023-07-28 20:31:54 +0200

    Some code fixes

--- Receipt Validator/Test data/test_params.py
+++ Receipt Validator/Test data/test_params.py
@@ -13,9 +13,8 @@ successful_response = 'Checking header content -- passed\nChecking the list of f
 no_content_error = 'RequestValidationError: Request has to contain a PDF file.'
 incorrect_format_error = 'RequestValidationError: File has another format then PDF.'
 # File validation error texts
-unexpected_header_error = f'Unexpected receipt header. Expected {header_reference} but actually '
-incorrect_fields_count_error = f'Fields validation failed: Count of the fields is wrong. Expected {len(fields_list_reference)} but actually '
-incorrect_fields_list_error = 'There were discrepancies when comparing the following fields with the reference:'
+unexpected_header_error = f'Unexpected receipt header. Expected: {header_reference}'
+unexpected_fields_list_error = f'Unexpected list of receipt fields. Expected: {fields_list_reference}'
 wrong_page_count_error = f'Expected number of pages {expected_page_count} but actually '
 barcode_errors = {'wrong_count': f'Barcode Validation failed: Wrong count of the barcodes in file. Expected {expected_barcode_count} but actually ',
                   'wrong_type': f'Wrong type of the barcode №',
--- Receipt Validator/receipt_validator.py
+++ Receipt Validator/receipt_validator.py
@@ -5,8 +5,8 @@ import barcode_validator
 from flask import Flask, request
 from uuid import uuid4
 from test_params import fields_regexp, header_regexp, expected_page_count, header_reference, fields_list_reference, \
-    wrong_page_count_error, unexpected_header_error, incorrect_fields_count_error, incorrect_fields_list_error, \
-    no_content_error, incorrect_format_error, successful_response
+    wrong_page_count_error, unexpected_header_error, no_content_error, incorrect_format_error, successful_response, \
+    unexpected_fields_list_error
 
 app = Flask(__name__)
 
@@ -31,26 +31,17 @@ def receipt_validator(path):
     extracted_text = receipt.pages[0].extract_text()
     header = re.findall(header_regexp, extracted_text)[0]
     fields_list = re.findall(fields_regexp, extracted_text)
-    fields_list_errors = incorrect_fields_list_error
     error_list = []
     # Validation
     if len(receipt.pages) != expected_page_count:
         error_list.append(f'{wrong_page_count_error}{len(receipt.pages)}')
     if header != header_reference:
-        error_list.append(f'{unexpected_header_error}{header}')
-    if len(fields_list_reference) == len(fields_list):
-        for i in range(len(fields_list_reference)):
-            if fields_list_reference[i] != fields_list[i]:
-                fields_list_errors += f' {fields_list[i]},'
-        if fields_list_errors != incorrect_fields_list_error:
-            error_list.append(fields_list_errors)
-    else:
-        error_list.append(f'{incorrect_fields_count_error}{len(fields_list)}')
+        error_list.extend([unexpected_header_error, f'Actually: {header}', ''])
+    print(fields_list_reference, fields_list, sep='\n')
+    if fields_list_reference != fields_list:
+        error_list.extend([unexpected_fields_list_error, f'Actually: {fields_list}'])
     error_list += barcode_validator.scan_and_validate(path)
-    if len(error_list) > 0:
-        return error_list
-    else:
-        return successful_response
+    return error_list if len(error_list) else successful_response
 
 
 if __name__ == '__main__':
